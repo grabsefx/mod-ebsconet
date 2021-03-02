804239e784a1702c097d1c51fbbdbeb3f37e1e78
MaksymShtanko <maksym_shtanko@epam.com>
2021-03-02 17:20:24 +0200

    [MODEBSNET-1] Add Orders Controller with no custom logic (rely on the default implementation).

--- src/main/java/org/folio/ebsconet/controller/OrdersController.java
+++ src/main/java/org/folio/ebsconet/controller/OrdersController.java
@@ -1,7 +1,14 @@
 package org.folio.ebsconet.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.ebsconet.rest.resource.OrdersApi;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/orders/order-lines")
 public class OrdersController implements OrdersApi {
-
 }
