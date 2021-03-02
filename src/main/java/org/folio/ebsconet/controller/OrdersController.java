4697445e6e48fcec6c3d3501bb3525426773e7b4
Andrei_Makaranka <Andrei_makaranka@epam.com>
2021-03-02 20:47:40 +0300

    [MODEBSNET-5] - Stubbing API ready

--- src/main/java/org/folio/ebsconet/controller/OrdersController.java
+++ src/main/java/org/folio/ebsconet/controller/OrdersController.java
@@ -1,7 +1,33 @@
 package org.folio.ebsconet.controller;
 
-import org.folio.ebsconet.rest.resource.OrdersApi;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
-public class OrdersController implements OrdersApi {
+import org.folio.ebsconet.domain.dto.PoLine;
+import org.folio.ebsconet.rest.resource.OrdersApi;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiParam;
+
+@RestController
+@RequestMapping(value = "/ebsconet")
+public class OrdersController  implements OrdersApi {
+
+  @Override
+  public ResponseEntity<PoLine> getPoLine(@Pattern(regexp="^[a-zA-Z0-9]{1,22}-[0-9]{1,3}$") @ApiParam(value = "product order line number",required=true) @PathVariable("poLineNumber") String poLineNumber) {
+    PoLine poLine = new PoLine();
+    poLine.setPoLineNumber(poLineNumber);
+
+    return new ResponseEntity<PoLine>(poLine, HttpStatus.OK);
+  }
+
+  public ResponseEntity<Void> putPoLine(@Pattern(regexp="^[a-zA-Z0-9]{1,22}-[0-9]{1,3}$") @ApiParam(value = "product order line number",required=true) @PathVariable("poLineNumber") String poLineNumber, @ApiParam(value = "" ,required=true )  @Valid @RequestBody PoLine poLine) {
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 
 }
